78cf764ce615dcbff3f9624a3112266d16470b0d
StephNereah <stphnadhiambo@gmail.com>
2024-07-28 14:41:35 +0300

    Base model structure

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# models/base_model.py
+
 """class BaseModel that defines all common attributes/methods for other classes
 """
 
@@ -25,6 +27,9 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = self.created_at
 
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     def save(self):
         """save method used for updating class so updated_at changes
         """
@@ -45,18 +50,3 @@ class BaseModel:
         """
         return "[{}] ({}) {}".format(
                 self.__class__.__name__, self.id, self.__dict__)
-
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(
-            key, type(my_model_json[key]), my_model_json[key]))
