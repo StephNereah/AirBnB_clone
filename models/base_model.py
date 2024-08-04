b4ec79ca71a1b06e790759a3c1a51e274575d28f
StephNereah <stphnadhiambo@gmail.com>
2024-08-04 07:59:42 -0700

    3. BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,52 +1,26 @@
 #!/usr/bin/python3
-# models/base_model.py
-
-"""class BaseModel that defines all common attributes/methods for other classes
 """
-
-import uuid
+class BaseModel that defines all common attributes/methods for other classes
+"""
 from datetime import datetime
+import uuid
 
 
 class BaseModel:
-    """base model parent class for all other classes
-       used in project
-    """
-
-    def __init__(self, *args, **kwargs):
-        """init method for base class used in instantiation
-        """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.fromisoformat(value)
-                if key != '__class__':
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-
-
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
     def save(self):
-        """save method used for updating class so updated_at changes
-        """
         self.updated_at = datetime.now()
-
+        
     def to_dict(self):
-        """returns the dictionary of our instance
-        """
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
-
-    def __str__(self):
-        """
-        custom str method for str and print
-        """
-        return "[{}] ({}) {}".format(
-                self.__class__.__name__, self.id, self.__dict__)
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
