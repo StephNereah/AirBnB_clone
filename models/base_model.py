b2ff95ab876d73fb81c238729112648a4659b122
StephNereah <stphnadhiambo@gmail.com>
2024-08-04 08:17:37 -0700

    3. BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -13,12 +13,23 @@ class BaseModel:
         self.updated_at = self.created_at
         
     def __str__(self):
+        """
+         string - assign with an uuid when an instance is created:
+         datetime -assign with the current datetime when an instance is created
+         it will be updated every time you change your object
+        """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     
     def save(self):
+        """
+        updates the public instance attribute updated_at with current datetime
+        """
         self.updated_at = datetime.now()
         
     def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__ of instance
+        """
         dict_copy = self.__dict__.copy()
         dict_copy['__class__'] = self.__class__.__name__
         dict_copy['created_at'] = self.created_at.isoformat()
